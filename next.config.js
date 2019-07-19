43b796ed82c09559335b739501d2c7a2be6ea417
kireeti <kireeti@accendo.com.my>
2019-07-19 14:30:51 +0800

    reverted to previous code

--- next.config.js
+++ next.config.js
@@ -88,8 +88,8 @@ module.exports = withCSS({
             },
             icons: [
               {
-                src: path.resolve('static/ios_splash.png'),
-                sizes: [96, 128, 192, 256, 384,512],
+                src: path.resolve('static/movie.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
                 destination: '/static',
               },{
                 src: path.resolve('static/movie.png'),
